91ed5995c17ad1b1dac28c8d584db646968f26f7
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-08-23 16:08:21 +0530

    anagram

---
+++ Anagram.cs
@@ -0,0 +1,31 @@
+﻿namespace Anagram
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string str1 = "Heart";
+            string str2 = "Earth";
+
+            char[] chr1 = str1.ToLower().ToCharArray();
+            char[] chr2 = str2.ToLower().ToCharArray();
+
+            Array.Sort(chr1);
+            Array.Sort(chr2);
+
+            string val1 = new string(chr1);
+            Console.WriteLine(val1);
+            string val2 = new string(chr2);
+            Console.WriteLine(val2);
+
+            if ( val1 == val2)
+            {
+                Console.WriteLine("Anagram");
+            }
+            else
+            {
+                Console.WriteLine("Not Anagram");
+            }
+        }
+    }
+}
