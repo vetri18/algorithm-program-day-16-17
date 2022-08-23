221e45a96725b8db51be489238be954f8633917d
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-08-23 16:07:28 +0530

    Bubble sort

---
+++ BubbleSort.cs
@@ -0,0 +1,29 @@
+﻿namespace BubbleSort
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int[] arr = { 78, 55, 45, 98, 13 };
+            int temp;
+            for( int j = 0; j <= arr.Length-2; j++ )
+            {
+                for ( int i = 0; i <= arr.Length-2; i++ )
+                {
+                    if ( arr[i] > arr[i + 1] )
+                    {
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine("Sorted Array");
+            foreach(int ele in arr )
+            {
+                Console.WriteLine(ele);
+            }
+        }
+    }
+}
